a1d987bcdad6a05aae1ebe6a45d6adbed0fd3f42
LuckDear <luccasm0804@gmail.com>
2024-03-12 16:08:06 -0300

    Tarefa modulo 9

---
+++ tarefa_Wrapper/ExercicioMod9/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+    public static void main(String[] args) {
+        long num1 = 355l;
+        System.out.println(num1 + " está definido como long");
+
+        System.out.println("Realizando Casting...");
+
+        Long num2 = num1;
+
+
+        System.out.println("Casting realizado, a variavel agora esta como wrapper com valor " + num2);
+    }
+}
